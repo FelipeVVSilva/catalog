3f035eac657d21c82ec886163794addf4532808d
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-12-11 14:55:10 -0300

    CategoryDTO created

---
+++ src/main/java/com/felipeveiga/catalog/entities/dto/CategoryDTO.java
@@ -0,0 +1,37 @@
+package com.felipeveiga.catalog.entities.dto;
+
+import java.io.Serializable;
+
+import com.felipeveiga.catalog.entities.Category;
+
+public class CategoryDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+
+	public CategoryDTO() {
+	}
+	public CategoryDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public CategoryDTO(Category cat) {
+		this.id = cat.getId();
+		this.name = cat.getName();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
