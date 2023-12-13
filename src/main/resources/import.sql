04e330cb8553b4ecf07a6c1743277bf94207b4e0
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-12-13 10:49:23 -0300

    database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_category(name, created_at) VALUES ('Boocks', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Electronics', NOW());
+INSERT INTO tb_category(name, created_at) VALUES ('Computers', NOW());
