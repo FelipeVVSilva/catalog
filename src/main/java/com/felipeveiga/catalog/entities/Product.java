2fdb3646a9c6719c78b77e17a1a440b9c7aa874b
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-12-19 13:14:34 -0300

    mapped product class

--- src/main/java/com/felipeveiga/catalog/entities/Product.java
+++ src/main/java/com/felipeveiga/catalog/entities/Product.java
@@ -1,16 +1,36 @@
 package com.felipeveiga.catalog.entities;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public class Product {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+			joinColumns = @JoinColumn(name = "product_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
