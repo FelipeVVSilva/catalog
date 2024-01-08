20a7b4483aa267aa5e60c3289b4a3979f5a0d475
Felipe.Silva <felipe.veiga.trabalho@gmail.com>
2024-01-08 17:20:54 -0300

    Create test of ProductRepository when save a new product and the id is null

--- src/test/java/com/felipeveiga/catalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/felipeveiga/catalog/repositories/ProductRepositoryTests.java
@@ -2,13 +2,14 @@ package com.felipeveiga.catalog.repositories;
 
 import java.util.Optional;
 
-import org.hibernate.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.felipeveiga.catalog.entities.Product;
+import com.felipeveiga.catalog.tests.ProductFactory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -16,15 +17,40 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repo;
 	
+	private long existingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	
+	
 	@Test
 	public void deleteShouldDeleteProductWhenIdExists() {
-		
+
 		Long existingId = 1L;
 		repo.deleteById(existingId);
-		
+
 		Optional<Product> result = repo.findById(existingId);
-		
+
 		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		
+		Product product = ProductFactory.createProduct();
+		product.setId(null);
+		
+		product = repo.save(product);
+		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
 		
 		
 	}
