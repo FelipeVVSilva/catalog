c3982665d210dfcceb0b281ca5ef64b05cb12fae
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2024-06-10 12:21:40 -0300

    Custom validation response to beans validation

--- src/main/java/com/felipeveiga/catalog/resources/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/felipeveiga/catalog/resources/exceptions/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,6 +30,19 @@ public class ControllerExceptionHandler {
 		StandardError error = new StandardError(Instant.now(), status.value(), "Database integrity violation", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(error);
 		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError error = new ValidationError(Instant.now(), status.value(), "Validation error", e.getMessage(), request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			error.addFieldError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(status).body(error);
 	}
 	
 }
